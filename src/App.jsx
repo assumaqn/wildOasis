ddac46ca17814f022f199003e2b2320f8b56cd09
unknown <somegetiye@gmail.com>
2026-02-10 04:47:21 -0800

    adding Global style to styled components

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,10 @@ import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyle";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
-const H1 = styled.h1`
-  font-size: 4rem;
-  font-weight: 600;
-`;
 const StyledApp = styled.main`
-  background-color: orange;
   padding: 20px;
 `;
 
@@ -17,11 +14,23 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <H1>Hello world</H1>
-        <Button>Check in </Button>
-        <Button>Check out</Button>
-        <Input type="number" placeholder="Number of guests" />
-        <Input type="number" placeholder="Number of guests" />
+        <Row>
+          <Heading as="h1">The wild oasis</Heading>
+          <div>
+            <Heading as="h2">Check in/out</Heading>
+            <Button>Check in</Button>
+            <Button sizes="small" variations="secondary">
+              Check out
+            </Button>
+          </div>
+        </Row>
+        <Row type="vertical">
+          <Heading as="h3">Form</Heading>
+          <div>
+            <Input type="number" placeholder="Number of guests" />
+            <Input type="number" placeholder="Number of guests" />
+          </div>
+        </Row>
       </StyledApp>
     </>
   );
