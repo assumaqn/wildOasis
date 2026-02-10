c23211f9b6a1d301a3f8919de0113c6e7fa86604
unknown <somegetiye@gmail.com>
2026-02-10 09:01:35 -0800

    adding the navigation

---
+++ src/ui/Logo.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const StyledLogo = styled.div`
+  text-align: center;
+`;
+
+const Img = styled.img`
+  height: 9.6rem;
+  width: auto;
+`;
+
+function Logo() {
+  return (
+    <StyledLogo>
+      <Link to="/">
+        <Img src="/logo-light.png" alt="Logo" />
+      </Link>
+    </StyledLogo>
+  );
+}
+
+export default Logo;
