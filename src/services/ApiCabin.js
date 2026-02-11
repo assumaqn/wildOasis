8c479d193281d19093ab78cc5dc42353b02225b7
unknown <somegetiye@gmail.com>
2026-02-11 15:52:34 -0800

    connecting our app to database

---
+++ src/services/ApiCabin.js
@@ -0,0 +1,10 @@
+import supabase from "./Supabase";
+
+export async function getCabins() {
+  const { data, error } = await supabase.from("cabins").select("*");
+  if (error) {
+    console.error(error);
+    throw new Error("cabins couldn't be loaded");
+  }
+  return data;
+}
---
+++ src/services/Supabase.js
@@ -0,0 +1,5 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://gsahaorpnzaaxrexgafm.supabase.co";
+const supabaseKey = "sb_publishable_FET_G35yzFZ40-S1fiuWAw__Xt4C9H0";
+const supabase = createClient(supabaseUrl, supabaseKey);
+export default supabase;
