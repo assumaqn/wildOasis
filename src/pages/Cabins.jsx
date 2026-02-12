05682b211c68ca4da42ebf5709badbfb025b2c4f
unknown <somegetiye@gmail.com>
2026-02-12 02:25:25 -0800

    fetching cabin data

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,21 +1,19 @@
-import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { getCabins } from "../services/ApiCabin";
+
+import CabinTable from "../features/cabins/CabinTable";
 
 function Cabins() {
-  useEffect(function () {
-    getCabins().then((data) => console.log(data));
-  }, []);
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-      <img
-        src="https://gsahaorpnzaaxrexgafm.supabase.co/storage/v1/object/public/Cabin-images/cabin-002.jpg"
-        alt="some image"
-      />
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter / Sort</p>
+      </Row>
+      <Row type="vertical">
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
